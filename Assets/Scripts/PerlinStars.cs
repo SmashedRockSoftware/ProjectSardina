012b5a4c8c84cbd749eb2ee108752b55d6278631
LilParker <bgray888@comcast.net>
2014-02-13 17:22:13 -0800

    Added basic star generator based on perlin noise.

---
+++ Assets/Scripts/PerlinStars.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PerlinStars : MonoBehaviour {
+
+	public float threshold;
+	public float roughness;
+	public float xRange;
+	public float yRange;
+	public GameObject star;
+	public List<GameObject> starList = new List<GameObject>();
+	public GameObject[] stars;
+
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x < xRange; x++){
+			for(int y = 0; y < yRange; y++){
+				if(Mathf.PerlinNoise(x + roughness, y + roughness) > threshold){
+					Instantiate(star, new Vector3(x, Random.Range(-1.0f, 1.0f), y), Quaternion.identity);
+				}
+			}
+		}
+	}
+}
