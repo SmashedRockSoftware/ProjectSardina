bbc1da22168dcd95298a4a8bbef3f285eb8c50ea
LilParker <bgray888@comcast.net>
2014-02-13 17:55:11 -0800

    Added placeholder star sprite, and some random variation in star size for illusion of depth

--- Assets/Scripts/PerlinStars.cs
+++ Assets/Scripts/PerlinStars.cs
@@ -17,7 +17,10 @@ public class PerlinStars : MonoBehaviour {
 		for(int x = 0; x < xRange; x++){
 			for(int y = 0; y < yRange; y++){
 				if(Mathf.PerlinNoise(x + roughness, y + roughness) > threshold){
-					Instantiate(star, new Vector3(x, Random.Range(-1.0f, 1.0f), y), Quaternion.identity);
+					GameObject starI = Instantiate(star, new Vector3(x + 0.5f * x, 0, y + 0.5f * y), Quaternion.identity) as GameObject;
+					starI.transform.Rotate(90, 0, 0);
+					float scaleAdd = Random.Range(-0.5f, 0.5f);
+					starI.transform.localScale = new Vector3(starI.transform.localScale.x + scaleAdd, starI.transform.localScale.y + scaleAdd, 0);
 				}
 			}
 		}
