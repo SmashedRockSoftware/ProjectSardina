58b57bfba275aa3207854c392cabc7e5f78c6645
tinykidtoo <tinykidtoodotcom@yahoo.com>
2014-02-13 19:12:19 -0800

    added limits to the editor and added to "test"

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -8,15 +8,17 @@ public class Camera : MonoBehaviour {
 
 	public Camera buildcam; //lets us choose the camera and reference it
 	public int speed = 10; //This lets us change the speed in the inspector
+	public int ZLimits = 46;//This locks it to the int + or - so it limits the camera
+	public int XLimits = 43;//This locks it to the int + or - so it limits the camera
 
 	void MoveBuildCam(){
-		if(transform.position.z >= 46) transform.Translate(Vector3.up * speed * -1 * Time.deltaTime); //This forced it to stay within bounds of 46 horizontaly
-		if(transform.position.z <= -46) transform.Translate(Vector3.up * speed * 1 * Time.deltaTime); //This forced it to stay within bounds of -46 horizontaly
-		if(transform.position.x >= 43) transform.Translate(Vector3.right * speed * -1 * Time.deltaTime); //This forced it to stay within bounds of 43 verticly
-		if(transform.position.x <= -43) transform.Translate(Vector3.right * speed * 1 * Time.deltaTime); //This forced it to stay within bounds of -43 verticly
+		if(transform.position.z >= ZLimits) transform.Translate(Vector3.up * speed * -1 * Time.deltaTime); //This forced it to stay within bounds of 46 horizontaly
+		if(transform.position.z <= -ZLimits) transform.Translate(Vector3.up * speed * 1 * Time.deltaTime); //This forced it to stay within bounds of -46 horizontaly
+		if(transform.position.x >= XLimits) transform.Translate(Vector3.right * speed * -1 * Time.deltaTime); //This forced it to stay within bounds of 43 verticly
+		if(transform.position.x <= -XLimits) transform.Translate(Vector3.right * speed * 1 * Time.deltaTime); //This forced it to stay within bounds of -43 verticly
 		
-		if(transform.position.z <= 46 && transform.position.z >= -46){ //If that this is in the right area let us control it
-			if(transform.position.x <= 43 && transform.position.x >= -43){ //If that this is in the right area let us control it
+		if(transform.position.z <= ZLimits && transform.position.z >= -ZLimits){ //If that this is in the right area let us control it
+			if(transform.position.x <= XLimits && transform.position.x >= -XLimits){ //If that this is in the right area let us control it
 				
 				float h = Input.GetAxis("Horizontal");//This is the number you edit to control the h axis
 				transform.Translate(Vector3.right * speed * h * Time.deltaTime);//This is then adjusted by that value
