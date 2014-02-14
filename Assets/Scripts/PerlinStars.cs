3301d18e3ad98e767fc5e97d5eeb449862801515
LilParker <bgray888@comcast.net>
2014-02-14 15:57:45 -0800

    Added more option to world generator

--- Assets/Scripts/PerlinStars.cs
+++ Assets/Scripts/PerlinStars.cs
@@ -8,6 +8,9 @@ public class PerlinStars : MonoBehaviour {
 	public float roughness;
 	public float xRange;
 	public float yRange;
+	public float spacing;
+	public float scaleRange;
+	public float offsetRange;
 	public GameObject star;
 	public List<GameObject> starList = new List<GameObject>();
 	public GameObject[] stars;
@@ -17,9 +20,11 @@ public class PerlinStars : MonoBehaviour {
 		for(int x = 0; x < xRange; x++){
 			for(int y = 0; y < yRange; y++){
 				if(Mathf.PerlinNoise(x + roughness, y + roughness) > threshold){
-					GameObject starI = Instantiate(star, new Vector3(x + 0.5f * x, 0, y + 0.5f * y), Quaternion.identity) as GameObject;
+					float xOffset = Random.Range(-offsetRange, offsetRange);
+					float yOffset = Random.Range(-offsetRange, offsetRange);
+					GameObject starI = Instantiate(star, new Vector3((x + spacing * x) + xOffset, 0, (y + spacing * y) + yOffset), Quaternion.identity) as GameObject;
 					starI.transform.Rotate(90, 0, 0);
-					float scaleAdd = Random.Range(-0.5f, 0.5f);
+					float scaleAdd = Random.Range(-scaleRange, scaleRange);
 					starI.transform.localScale = new Vector3(starI.transform.localScale.x + scaleAdd, starI.transform.localScale.y + scaleAdd, 0);
 				}
 			}
