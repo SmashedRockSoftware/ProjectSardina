e96f07c1ca5450078e152c551123c9118d97b378
LilParker <bgray888@comcast.net>
2014-02-15 08:37:55 -0800

    Planetary Systems base

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	public GameObject Planet;
+	public Planet[] planets;
+	public int planetLimit;
+	public float orbitalVariation;
+	public float orbitalMult;
+
+	// Use this for initialization
+	void Start () {
+		PlanetaryGenerator();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void PlanetaryGenerator () {
+		int planetNumber = Random.Range(0, planetLimit);
+		planets = new Planet[planetNumber];
+		for(int i = 0; i < planetNumber; i++){
+			planets[i].radius = i * orbitalMult + Random.Range(-orbitalVariation, orbitalVariation);
+			planets[i].planet = Planet;
+		}
+	}
+}
