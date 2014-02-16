129b310cbab6c6341ba6687bba772ad2f2131dc4
LilParker <bgray888@comcast.net>
2014-02-15 19:16:55 -0800

    More work on planet generaton

--- Assets/Scripts/Star.cs
+++ Assets/Scripts/Star.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class Star : MonoBehaviour {
 
-	public GameObject Planet;
+	public GameObject[] PlanetList = new GameObject[2];
 	public Planet[] planets;
 	public int planetLimit;
 	public float orbitalVariation;
@@ -27,9 +27,9 @@ public class Star : MonoBehaviour {
 		planets = new Planet[planetNumber];
 		for(int i = 0; i < planetNumber; i++){
 			planets[i] = new Planet();
-			planets[i].radius = 100/(i + 1) * Random.Range(-orbitalVariation, orbitalVariation);
-			planets[i].planet = Planet;
-			planets[i].orbitSpeed = Random.Range(0.5f, orbitalSpeedVariation);
+			planets[i].radius = 100/(i + 1) * Random.Range(-orbitalVariation, orbitalVariation) + 2;
+			planets[i].planet = PlanetList[Random.Range(0, PlanetList.Length)];
+			planets[i].orbitSpeed = Random.Range(0.5f, orbitalSpeedVariation)/planets[i].radius;
 		}
 	}
 
@@ -42,8 +42,8 @@ public class Star : MonoBehaviour {
 		AudioListener listenerPlanet = SystemStar.planetCam.gameObject.GetComponent<AudioListener>() as AudioListener;
 		listenerPlanet.enabled = true;
 		for(int i = 0; i < planets.Length; i++){
-			SystemStar.planets[i].transform.position = new Vector3(Random.Range(-100.0f, 100.0f), 0, Random.Range(-100.0f, 100.0f)).normalized * planets[i].radius;
-			SystemStar.planets[i].transform.position += new Vector3(998 - 2 * i, 0, 0);
+			SystemStar.planets[i].transform.position = new Vector3(1000, 0, 0);
+			SystemStar.planets[i].transform.position += new Vector3(Random.Range(-100.0f, 100.0f), 0, Random.Range(-100.0f, 100.0f)).normalized * planets[i].radius;
 			SpriteRenderer sprite = SystemStar.planets[i].GetComponent<SpriteRenderer>() as SpriteRenderer;
 			SpriteRenderer spritePlanet = planets[i].planet.GetComponent<SpriteRenderer>() as SpriteRenderer;
 			sprite.sprite = spritePlanet.sprite;
