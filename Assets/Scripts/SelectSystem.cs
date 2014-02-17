1f119434b47505971d9b829202499422eefb71eb
LilParker <bgray888@comcast.net>
2014-02-16 21:47:08 -0800

    Added basic unit movement

--- Assets/Scripts/SelectSystem.cs
+++ Assets/Scripts/SelectSystem.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class SelectSystem : MonoBehaviour {
 
 	Star star;
+	MoveShip ship;
+	
 
 	// Update is called once per frame
 	void Update () {
@@ -15,10 +17,24 @@ public class SelectSystem : MonoBehaviour {
 					star = hit.transform.gameObject.GetComponent<Star>() as Star;
 					star.LoadSystem();
 				}
+				if(hit.transform.gameObject.GetComponent<MoveShip>() != null){
+					ship = hit.transform.gameObject.GetComponent<MoveShip>() as MoveShip;
+				}
 			}
 		}
 		if(Input.GetButton("Fire2") && Camera.main == null){
 			star.UnloadSystem();
+		}
+		if(Input.GetButton("Fire2") && Camera.main != null){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)){
+				if(ship != null){
+					if(hit.transform.gameObject.GetComponent<Star>() != null){
+						ship.ShipMover(hit.transform.gameObject.transform.position + new Vector3(0, 1, 0.5f), 0.25f);
+					}
+				}
+			}
 		}
 	}
 }
